1bc15cf9854b3c550120702fd6ae2a49b15ae0f9
Pranati <pranati05@gmail.com>
2026-09-17 13:54:53 -0700

    Container with most water

---
+++ Container_with_most_water.py
@@ -0,0 +1,24 @@
+# Time Complexity : O(N)
+# Space Complexity : O(1)
+# Did this code successfully run on Leetcode : Yes
+# Any problem you faced while coding this : No
+
+
+# Your code here along with comments explaining your approach
+#I have used two pointer approach to reduce from n2 time to n. Using left and right pointer we can get the min height to contain the water
+#Then calculate the area by multiplying minimum height
+
+class Solution:
+    def maxArea(self, height: list[int]) -> int:
+        left = 0
+        right = len(height)-1
+        maxArea = 0
+        while left < right:
+            minheight = min(height[left], height[right])
+            currentArea = (right - left) * minheight
+            maxArea = max(currentArea,maxArea)
+            if height[left] < height[right]:
+                left += 1
+            else:
+                right -= 1
+        return maxArea
